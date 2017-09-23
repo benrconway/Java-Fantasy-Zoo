da1330abbe0eeaa18b3b5beb6248749812e60c33
Benjamin Conway <benrconway84@gmail.com>
2017-09-23 19:32:59 +0100

    Continue testing Environment Class

--- app/src/main/java/com/example/user/fantasyzooapp/people/Person.java
+++ app/src/main/java/com/example/user/fantasyzooapp/people/Person.java
@@ -1,10 +1,13 @@
 package com.example.user.fantasyzooapp.people;
 
+import com.example.user.fantasyzooapp.food.Edible;
+import com.example.user.fantasyzooapp.food.Flesh;
+
 /**
  * Created by user on 22/09/2017.
  */
 
-public abstract class Person {
+public abstract class Person implements Edible, Flesh {
     private String name;
     private int wallet;
     private int skill;
