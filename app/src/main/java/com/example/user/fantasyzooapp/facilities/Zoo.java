ac4fb9b8e9a63e8823063e2d36ddc4477bb9faba
Benjamin Conway <benrconway84@gmail.com>
2017-09-25 10:54:12 +0100

    design functions of Zoo class

--- app/src/main/java/com/example/user/fantasyzooapp/facilities/Zoo.java
+++ app/src/main/java/com/example/user/fantasyzooapp/facilities/Zoo.java
@@ -1,9 +1,83 @@
 package com.example.user.fantasyzooapp.facilities;
 
+import com.example.user.fantasyzooapp.animals.Animal;
+import com.example.user.fantasyzooapp.food.Flesh;
+import com.example.user.fantasyzooapp.food.Vegetation;
+import com.example.user.fantasyzooapp.people.Person;
+
+import java.util.ArrayList;
+
 /**
  * Created by user on 25/09/2017.
  */
 
 public class Zoo {
+
+    private boolean gatesAreOpen;
+    private int customerCapacity;
+    private int ticketPrice;
+    private int funds;
+    private int day;
+
+    private ArrayList<Person> roaming;
+    private ArrayList<Vegetation> vegetableStock;
+    private ArrayList<Flesh> meatStock;
+    private ArrayList<Animal> loose;
+
+    public Zoo() {
+        gatesAreOpen = false;
+        customerCapacity = 100;
+        ticketPrice = 10;
+        funds = 1_000_000;
+        day = 0;
+        roaming = new ArrayList<>();
+        vegetableStock = new ArrayList<>();
+        meatStock = new ArrayList<>();
+        loose = new ArrayList<>();
+    }
+
+    public boolean isGatesAreOpen() {
+        return gatesAreOpen;
+    }
+
+    public int getCustomerCapacity() {
+        return customerCapacity;
+    }
+
+    public int getTicketPrice() {
+        return ticketPrice;
+    }
+
+    public int getFunds() {
+        return funds;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public ArrayList<Person> getRoaming() {
+        return roaming;
+    }
+
+    public ArrayList<Vegetation> getVegetableStock() {
+        return vegetableStock;
+    }
+
+    public ArrayList<Flesh> getMeatStock() {
+        return meatStock;
+    }
+
+    public ArrayList<Animal> getLoose() {
+        return loose;
+    }
+
+    public void openGates() {
+        gatesAreOpen = true;
+    }
+
+    public void closeGates() {
+        gatesAreOpen = false;
+    }
 
 }
