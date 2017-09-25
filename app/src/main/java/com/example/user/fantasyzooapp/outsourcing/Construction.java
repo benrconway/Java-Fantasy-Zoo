eff45ee7d5e73211b606350cac5a0cef3a5cafbd
Benjamin Conway <benrconway84@gmail.com>
2017-09-25 14:00:17 +0100

    Just to be sure.

---
+++ app/src/main/java/com/example/user/fantasyzooapp/outsourcing/Construction.java
@@ -0,0 +1,18 @@
+package com.example.user.fantasyzooapp.outsourcing;
+
+import com.example.user.fantasyzooapp.facilities.Building;
+
+import java.util.ArrayList;
+
+/**
+ * Created by user on 25/09/2017.
+ */
+
+public class Construction {
+
+    private ArrayList<Building> buildingsAvailable;
+
+    public Construction(){
+        buildingsAvailable = new ArrayList<>();
+    }
+}
